b945bf7667ed717119c783d5eb653bd741e987e9
geksafobiya <asja653@gmail.com>
2021-05-24 15:52:34 +0300

    четырехмерный случай с сохраняемыми вариантами

--- options_menu_4_species.py
+++ options_menu_4_species.py
@@ -6,25 +6,41 @@ import PyQt5.QtWidgets as QtWidgets
 
 class OptionsMenu_4_species(QtWidgets.QWidget):
     def __init__(self, parent=None):
+        self.settings_path = 'settings\\coef.json'
+
         QtWidgets.QWidget.__init__(self, parent)
 
         # Create the "Lotka-Volterra Coefficients" options
         self.b1_sb = QtWidgets.QDoubleSpinBox()
+        self.b1_sb.setObjectName("b1_sb")
         self.a11_sb = QtWidgets.QDoubleSpinBox()
+        self.a11_sb.setObjectName("a11_sb")
         self.a12_sb = QtWidgets.QDoubleSpinBox()
+        self.a12_sb.setObjectName("a12_sb")
         self.a13_sb = QtWidgets.QDoubleSpinBox()
+        self.a13_sb.setObjectName("a13_sb")
         self.a14_sb = QtWidgets.QDoubleSpinBox()
+        self.a14_sb.setObjectName("a14_sb")
 
         self.b2_sb = QtWidgets.QDoubleSpinBox()
+        self.b2_sb.setObjectName("b2_sb")
         self.a21_sb = QtWidgets.QDoubleSpinBox()
+        self.a21_sb.setObjectName("a21_sb")
         self.a22_sb = QtWidgets.QDoubleSpinBox()
+        self.a22_sb.setObjectName("a22_sb")
         self.a23_sb = QtWidgets.QDoubleSpinBox()
+        self.a23_sb.setObjectName("a23_sb")
 
         self.b3_sb = QtWidgets.QDoubleSpinBox()
+        self.b3_sb.setObjectName("b3_sb")
         self.a31_sb = QtWidgets.QDoubleSpinBox()
+        self.a31_sb.setObjectName("a31_sb")
         self.a32_sb = QtWidgets.QDoubleSpinBox()
+        self.a32_sb.setObjectName("a32_sb")
         self.a33_sb = QtWidgets.QDoubleSpinBox()
+        self.a33_sb.setObjectName("a33_sb")
         self.b4_sb = QtWidgets.QDoubleSpinBox()
+        self.b4_sb.setObjectName("b4_sb")
         #self.a41_sb = QtWidgets.QDoubleSpinBox()
         #self.a42_sb = QtWidgets.QDoubleSpinBox()
    # = QtWidgets.QDoubleSpinBox()
@@ -37,6 +53,7 @@ class OptionsMenu_4_species(QtWidgets.QWidget):
             widget.setSingleStep(0.05)
 
         coeff_grid = QtWidgets.QGridLayout()
+        self.coeff_grid = coeff_grid
         coeff_grid.addWidget(QtWidgets.QLabel('Рождаемость жертв вида 1'), 0, 0)
         coeff_grid.addWidget(self.b1_sb, 0, 1)
         coeff_grid.addWidget(QtWidgets.QLabel('Рождаемость жертв вида 2'), 1, 0)
@@ -76,11 +93,12 @@ class OptionsMenu_4_species(QtWidgets.QWidget):
         self.predator_sb = QtWidgets.QDoubleSpinBox()
         self.predator_sb.setRange(0, 100000)
         self.predator_sb.setSingleStep(1)
+        self.predator_sb.setObjectName("predator_sb")
 
         self.superpredator_sb = QtWidgets.QDoubleSpinBox()
         self.superpredator_sb.setRange(0, 100000)
         self.superpredator_sb.setSingleStep(1)
-
+        self.superpredator_sb.setObjectName("superpredator_sb")
   #      self.predator3_sb = QtWidgets.QDoubleSpinBox()
    #     self.predator3_sb.setRange(0, 100000)
    #     self.predator3_sb.setSingleStep(1)
@@ -91,10 +109,12 @@ class OptionsMenu_4_species(QtWidgets.QWidget):
         self.prey1_sb = QtWidgets.QDoubleSpinBox()
         self.prey1_sb.setRange(0, 100000)
         self.prey1_sb.setSingleStep(1)
+        self.prey1_sb.setObjectName("prey1_sb")
 
         self.prey2_sb = QtWidgets.QDoubleSpinBox()
         self.prey2_sb.setRange(0, 100000)
         self.prey2_sb.setSingleStep(1)
+        self.prey2_sb.setObjectName("prey2_sb")
 
         self.iterations_sb = QtWidgets.QSpinBox()
         self.iterations_sb.setRange(0, 1000000)
@@ -178,19 +198,37 @@ class OptionsMenu_4_species(QtWidgets.QWidget):
 
         self.reset_values_btn.clicked.connect(self.reset_values)
 
+        main_actions_vbox = QtWidgets.QVBoxLayout()
+        main_actions_vbox.addWidget(self.update_btn)
+        main_actions_vbox.addWidget(self.reset_values_btn)
+        main_actions_vbox.addWidget(self.clear_graph_btn)
+
+        self.save_btn = QtWidgets.QPushButton('save')
+        self.save_btn.clicked.connect(self.save_coeff)
+        self.load_drop_box = QtWidgets.QComboBox()
+        self.load_drop_box.setEditable(True)
+        self.load_drop_box.currentIndexChanged.connect(self.load_coeff)
+
+        store_load_settings_vbox = QtWidgets.QVBoxLayout()
+        store_load_settings_vbox.addWidget(self.save_btn)
+        store_load_settings_vbox.addWidget(self.load_drop_box)
+        store_load_settings_vbox.addStretch()
+
+        main_actions_hbox = QtWidgets.QHBoxLayout()
+        main_actions_hbox.addLayout(store_load_settings_vbox)
+        main_actions_hbox.addLayout(main_actions_vbox)
+
         # Create the main layout
         container = QtWidgets.QVBoxLayout()
         container.addWidget(coeff_gb)
         container.addWidget(other_gb)
         container.addWidget(graph_gb)
-        container.addWidget(self.update_btn)
-        container.addStretch()
-        container.addWidget(self.reset_values_btn)
-        container.addWidget(self.clear_graph_btn)
+        container.addLayout(main_actions_hbox)
         self.setLayout(container)
 
         # Populate the widgets with values
         self.reset_values()
+        self.load_coeff_names()
 
     def reset_values(self):
         """
@@ -230,3 +268,92 @@ class OptionsMenu_4_species(QtWidgets.QWidget):
     def legend_change(self):
         self.legend_loc_cb.setEnabled(self.legend_cb.isChecked())
         self.legend_loc_lbl.setEnabled(self.legend_cb.isChecked())
+
+    def load_coeff(self):
+        current_idx = self.load_drop_box.currentIndex()
+
+        if current_idx == 0:
+            return
+
+        coeff_preset_list = self.readSettings()
+        for preset in coeff_preset_list:
+            preset_name = list(preset.keys())[0]
+
+            if preset_name == self.load_drop_box.currentText():
+                coeff_dict = preset[preset_name]
+                self.b1_sb.setValue(coeff_dict['b1_sb'])
+                self.a11_sb.setValue(coeff_dict['a11_sb'])
+                self.a12_sb.setValue(coeff_dict['a12_sb'])
+                self.a13_sb.setValue(coeff_dict['a13_sb'])
+                self.a14_sb.setValue(coeff_dict['a14_sb'])
+
+                self.b2_sb.setValue(coeff_dict['b2_sb'])
+                self.a21_sb.setValue(coeff_dict['a21_sb'])
+                self.a22_sb.setValue(coeff_dict['a22_sb'])
+                self.a23_sb.setValue(coeff_dict['a23_sb'])
+
+                self.b3_sb.setValue(coeff_dict['b3_sb'])
+                self.a31_sb.setValue(coeff_dict['a31_sb'])
+                self.a32_sb.setValue(coeff_dict['a32_sb'])
+                self.a33_sb.setValue(coeff_dict['a33_sb'])
+
+                self.b4_sb.setValue(coeff_dict['b4_sb'])
+
+    def load_coeff_names(self):
+        coeff_preset_list = self.readSettings()
+        self.load_drop_box.clear()
+        self.load_drop_box.addItem('')
+
+        if len(coeff_preset_list) == 0:
+            return
+
+        for preset in coeff_preset_list:
+            self.load_drop_box.addItem(list(preset.keys())[0])
+
+    def save_coeff(self):
+        import json
+        import os
+
+        preset_name = self.load_drop_box.currentText().strip()
+        if len(preset_name) == 0:
+            print('Пустое имя для пресета с коэфициентами')
+            return
+
+        coeff_preset_list = self.readSettings()
+
+        coeff_dict = dict()
+        for widget in (self.b1_sb, self.a11_sb, self.a12_sb, self.a13_sb, self.a14_sb,
+                       self.b2_sb, self.a21_sb, self.a22_sb, self.a23_sb,
+                       self.b3_sb, self.a31_sb, self.a32_sb, self.a33_sb,
+                       self.b4_sb):
+            if len(widget.objectName()) == 0:
+                print('нет имя для переменной со значением: ', widget.value())
+                continue
+            coeff_dict[widget.objectName()] = widget.value()
+
+        coeff_preset_list.append({preset_name: coeff_dict})
+        self.writeSettings(coeff_preset_list)
+        self.load_coeff_names()
+
+    def readSettings(self) -> list:
+        import json
+        import os
+
+        if not os.path.exists(self.settings_path):
+            os.mkdir('settings')
+
+        try:
+            with open(self.settings_path) as json_file:
+                coeff_preset_list = json.load(json_file)
+        except FileNotFoundError as ex:
+            coeff_preset_list = list()
+
+        return coeff_preset_list
+
+    def writeSettings(self, coeff_preset_list):
+        import json
+        import os
+
+        json_coeff = json.dumps(coeff_preset_list, indent=2)
+        with open(self.settings_path, 'w') as file:
+            file.write(json_coeff)
